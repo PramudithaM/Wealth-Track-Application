dc3620477534a6979add62c3d192cd5e492b0e65
pramuditha_madura <pramuditham8@gmail.com>
2026-01-19 14:55:58 +0530

    Update Backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,9 +7,13 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="WealthTrack API", version="1.0.0")
 
+origins = [
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
